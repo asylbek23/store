cc39b491549963a8ec5536823347399c2a589542
Asylbek <asylbek2323@gmail.com>
2023-09-03 15:06:11 +0600

    Просмотр продуктов

--- src/views/ProductDetails/ProductDetails.vue
+++ src/views/ProductDetails/ProductDetails.vue
@@ -1,12 +1,15 @@
 <template>
-  <div :class="$style.product">
+  <div :class="$style['product-page']">
     <div class="container">
-      <h1>Product details</h1>
-      <div class="product-details__content">
-        <img :src="product.image" :alt="product.title" />
-        <h2 class="title">{{ product.title }}</h2>
-        <div class="price">{{ product.price }}</div>
-        <p class="descr">{{ product.description }}</p>
+      <h1 class="title">Product details</h1>
+      <div :class="$style.product">
+        <img class="product-img" :src="product.image" :alt="product.title" />
+        <h2 class="product-title">{{ product.title }}</h2>
+        <div class="product-price">
+          Price:
+          <span>{{ product.price }}$</span>
+        </div>
+        <p class="product-descr">{{ product.description }}</p>
       </div>
     </div>
   </div>
